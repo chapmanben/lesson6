c895d88580c312c66b15098d93c6dcff764daa05
Benjamin Chapman <benjaminchapman@rogers.com>
2015-06-11 13:58:34 -0400

    added departments, completed student CRUD

--- comp2007-wed1-Lesson5/student.aspx
+++ comp2007-wed1-Lesson5/student.aspx
@@ -7,20 +7,21 @@
     <h5>All fields are required</h5>
     <fieldset>
         <div>
-            <label for="txtLastName">Last Name:</label>
+            <label for="txtLastName" class="col-sm-2">Last Name:</label>
             <asp:TextBox ID="txtLastName" runat="server" required MaxLength="50" />
         </div>
         <div>
-            <label for="txtFirstMidName">First Name:</label>
+            <label for="txtFirstMidName" class="col-sm-2">First Name:</label>
             <asp:TextBox ID="txtFirstMidName" runat="server" required MaxLength="50" />
         </div>
         <div>
-            <label for="txtEnrollmentDate">Enrollment Date</label>
+            <label for="txtEnrollmentDate" class="col-sm-2">Enrollment Date</label>
             <asp:TextBox ID="txtEnrollmentDate" runat="server" required TextMode="Date"/>
             <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Must Be A Date" 
                     ControlToValidate="txtEnrollmentDate" CssClass="alert alert-danger" Type="Date" MinimumValue="01/01/2000" MaximumValue="12/31/2999"></asp:RangeValidator>
         </div>
     </fieldset>
-
-    <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click"/>
+    <div class="col-sm-offset-2">
+    <asp:Button ID="btnSave"  runat="server" Text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click"/>
+    </div>
 </asp:Content>
--- comp2007-wed1-Lesson5/student.aspx.cs
+++ comp2007-wed1-Lesson5/student.aspx.cs
@@ -15,7 +15,35 @@ namespace comp2007_wed1_Lesson5
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if sav wasn't clicked & we have a student ID in the url.
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                getStudent();
+            }
+            
+        }
 
+        protected void getStudent() { 
+        //populate the form with student information
+            Int32 studentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+            using (gc200261581Entities1 db = new gc200261581Entities1()){
+                Student s = (from objs in db.Students
+                                 where objs.StudentID == studentID
+                                 select objs).FirstOrDefault();
+                if (s != null)
+                {
+                    txtFirstMidName.Text = s.FirstMidName;
+                    txtLastName.Text = s.LastName;
+                    txtEnrollmentDate.Text = s.EnrollmentDate.ToString("mm-dd-yyyy");
+                }
+                //else
+                //{
+                //    Response.Redirect("students.aspx");
+                //}
+
+
+            }
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
@@ -23,14 +51,29 @@ namespace comp2007_wed1_Lesson5
             //use EF to connect to SQL server
             using (gc200261581Entities1 db = new gc200261581Entities1())
             {
-                //use student model to save new student 
+                
                 Student s = new Student();
+                Int32 StudentID = 0;
+                //check the query string for an ID. To determine add or update
+                if (Request.QueryString["StudentID"] != null)
+                {
+                    //get id from url
+                    StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
 
+                    //get the current student from EF
+                    s = (from objs in db.Students
+                         where objs.StudentID == StudentID
+                         select objs).FirstOrDefault();
+                }
+
+                //use student model to save new student 
                 s.LastName = txtLastName.Text;
                 s.FirstMidName = txtFirstMidName.Text;
                 s.EnrollmentDate = Convert.ToDateTime(txtEnrollmentDate.Text);
 
+                if(StudentID == 0){ 
                 db.Students.Add(s);
+                }
                 db.SaveChanges();
             }
 
